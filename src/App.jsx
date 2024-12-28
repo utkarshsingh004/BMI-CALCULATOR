a29b45c2e4ce792af8fcc5c88feef06c67c7269e
utkarshsingh004 <singhutkarshkumar097@gmail.com>
2024-12-28 20:17:04 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import BmiCalculator from './BmiCalculator';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <BmiCalculator />
+    </div>
+  );
+}
+
+export default App;
